6cadf66ab79f203396d8f012f3a42a49ee8e51fc
AG <art.gromov@bk.ru>
2021-08-16 16:27:45 +0300

    improve plot highs and lows T Death Valley and shading an area

--- Data Analize Weather/Rain_visual.py
+++ Data Analize Weather/Rain_visual.py
@@ -7,26 +7,21 @@ filename = 'sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    # Get dates, and high and low temperatures from this file.
-    dates, highs, lows = [], [], []
+    # Get dates, and rainfall amounts from this file.
+    dates, rains = [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
+        rain = float(row[3])
         dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        rains.append(rain)
 
     # Plot the high and low temperatures.
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    ax.plot(dates, highs, c='red', alpha=0.5)
-    ax.plot(dates, lows, c='blue', alpha=0.5)
-    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+    ax.plot(dates, rains, c='red')
     # Format plot.
-    plt.title("Daily high and low temperatures - 2018", fontsize=24)
+    plt.title("Daily rainfall amounts - 2018", fontsize=24)
     plt.xlabel('', fontsize=16)
-    fig.autofmt_xdate()
-    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.ylabel("Rainfall amounts", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
     plt.show()
